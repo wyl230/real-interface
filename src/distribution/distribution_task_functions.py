236046b4a49581480c77110e4abc42f58a85abe0
wyl230 <1900012779@pku.edu.cn>
2023-06-18 18:02:28 +0800

    添加多条业务流同时运行，认为是均匀分布 && 添加单中心分布

--- src/distribution/distribution_task_functions.py
+++ src/distribution/distribution_task_functions.py
@@ -50,7 +50,7 @@ def location_config(location_par):
         # 生成用户分布
         if model_type == '2':
             if distribution_type == '1':
-                distribution_result = uniform_distribution(center_longitude, center_latitude, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype)
+                distribution_result = monocentric_distribution(center_longitude, center_latitude, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype, center_scat=0.1)
             elif distribution_type == '2':
                 distribution_result = uniform_distribution(center_longitude, center_latitude, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype)
             elif distribution_type == '3':
@@ -95,6 +95,72 @@ def uniform_distribution(lon_0, lat_0, ue_type, radius, ue_num, ue_id, loc_confi
         ue_id[0] += 1
         loc_config_res.append(tmp_dict)
 
+def monocentric_distribution(lon_0, lat_0, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype, center_scat):
+    earth_radius = 6371  # 地球半径，单位为公里
+    # 将经度、纬度转换为弧度
+    lat_rad = np.radians(lat_0)
+    lon_rad = np.radians(lon_0)
+
+    # 生成中心点的位置
+    center_distance = np.random.uniform(0, (1 - center_scat)*radius, 1)
+    center_angle = np.random.uniform(0, 2*np.pi, 1)
+    center_delta_lat = np.arcsin(np.sin(lat_rad) * np.cos(center_distance / earth_radius) +
+                                 np.cos(lat_rad) * np.sin(center_distance / earth_radius) * np.cos(center_angle))
+    center_delta_lon = lon_rad + np.arctan2(np.sin(center_angle) * np.sin(center_distance / earth_radius) * np.cos(lat_rad),
+                                            np.cos(center_distance / earth_radius) - np.sin(lat_rad) * np.sin(center_delta_lat))
+    
+    # print("分布中心点位置：", np.degrees(center_delta_lon),np.degrees(center_delta_lat))
+    
+    # 生成num_points个在圆形范围内均匀分布的随机角度和距离
+    center_scat_distance = center_scat * radius * np.random.rand(1, ue_num)
+    center_scat_angle = np.random.uniform(0, 2*np.pi, ue_num)
+
+    # 使用Haversine公式计算坐标
+    delta_lat = np.arcsin(np.sin(center_delta_lat) * np.cos(center_scat_distance / earth_radius) +
+                          np.cos(center_delta_lat) * np.sin(center_scat_distance / earth_radius) * np.cos(center_scat_angle))
+    delta_lon = center_delta_lon + np.arctan2(np.sin(center_scat_angle) * np.sin(center_scat_distance / earth_radius) * np.cos(center_delta_lat),
+                                               np.cos(center_scat_distance / earth_radius) - np.sin(center_delta_lat) * np.sin(delta_lat))
+
+    # 将经度、纬度转换为度数
+    lat_deg = np.degrees(delta_lat)
+    lon_deg = np.degrees(delta_lon)
+
+    for i in range(ue_num):
+        # 纬度跨半球处理
+        if lat_deg[0,i] > 90:
+            lat_deg[0,i] = 180 - lat_deg[0,i]
+        elif lat_deg[0,i] < -90:
+            lat_deg[0,i] = -180 - lat_deg[0,i]
+        # 经度跨半球处理
+        if lon_deg[0,i] > 180:
+            lon_deg[0,i] -= 360
+        elif lon_deg[0,i] < -180:
+            lon_deg[0,i] += 360
+        # 将结果编辑为字典添加进distribution_result
+        tmp_dict = {
+            #'terminalId': ue_id[0],
+            #'terminalName': '终端_' + str(ue_id[0]),
+            'terminalType': ue_type,
+            'locationType': ue_loctype,
+            'longitude': lon_deg[0,i],
+            'latitude': lat_deg[0,i]
+        }
+        #ue_id[0] += 1
+        loc_config_res.append(tmp_dict)
+
+    return lat_deg, lon_deg
+
+
+
+def single_center_centralized_distribution(lon_0, lat_0, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype):
+    pass 
+
+def multicenter_centralized_distribution(lon_0, lat_0, ue_type, radius, ue_num, ue_id, loc_config_res, ue_loctype):
+    pass 
+
+def multicenter_random_distribution():
+    pass 
+
 # 业务配置函数
 def task_config(task_par):
     # 解析为python字典
@@ -104,20 +170,17 @@ def task_config(task_par):
     task_time = par.timeRange
     start_time = task_time[0] / 1000
     end_time = task_time[1] / 1000
-    # 任务时间分布类型
-    time_type = par.time
-    # 总业务数
-    task_num = par.totalNum
-    # 业务组成
-    task_composition = par.composition
+    time_type = par.time # 任务时间分布类型 均匀分布、泊松分布
+    task_num = par.totalNum # 总业务数
+    task_composition = par.composition # 业务组成
 
     # 业务配置返回值
     biz_data = []
     # 生成视频业务配置
     for biz_comp in task_composition:
         # todo
-        if time_type == '1':
-            biz_config(biz_comp, task_num, biz_data, start_time, end_time)
+        if time_type == '1': # 均匀分布
+            multi_flow_biz_config(biz_comp, task_num, biz_data, start_time, end_time)
         if time_type == '2':
             biz_config(biz_comp, task_num, biz_data, start_time, end_time)
 
@@ -133,6 +196,19 @@ def id_analysis(ue):
     id1 = int(numbers[0])
     id2 = int(numbers[1])
     return (id1, id2)
+
+def multi_flow_biz_config(biz_comp, task_num, biz_data, start_time, end_time):
+    for i in range(0, task_num):
+        # 生成一对收发端id
+        source_id, dest_id = id_gen()
+        tmpdict = {
+            'startTime': start_time,
+            'endTime': end_time,
+            'source': source_id,
+            'destination': dest_id,
+            'bizType': biz_comp.bizType
+        }
+        biz_data.append(tmpdict)
 
 # 业务泊松分布
 def biz_config(biz_comp, task_num, biz_data, start_time, end_time, biz_duration=180, max_biz=1):
@@ -158,9 +234,6 @@ def biz_config(biz_comp, task_num, biz_data, start_time, end_time, biz_duration=
 
     n = len(biz_starttime)
     # 处理返回值
-
-
-
 
     for i in range(0, n):
         # 生成一对收发端id
