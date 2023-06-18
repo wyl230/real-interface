25dc91e9bcebcdc59cbac4dc4a0d52c1923c5a71
wyl230 <1900012779@pku.edu.cn>
2023-06-18 14:29:02 +0800

    map count问题，clear之后不再访问

--- test_stream_running_on_188_k8s_max_num.py
+++ test_stream_running_on_188_k8s_max_num.py
@@ -3,8 +3,8 @@ import time
 t1 = '''curl -X POST -H "Content-Type: application/json" -d '{ "param": [ { "paramType": "baseTime", "paramName": "realTime", "paramValue": "''' + str(round(time.time() * 1000)) + '''"}, { "paramType": "baseTime", "paramName": "simulationTime", "paramValue": "0" } ] }' http://162.105.85.188:32415/xw/param/config'''
 
 
-flow_num = 50
-s = [f""" {{ "insId" :{cnt}, "startTime": 2000, "endTime": 10000000, "source": 153, "destination": 283, "bizType": "1"  }} """ for cnt in range(1, flow_num)]
+flow_num = 5
+s = [f""" {{ "insId" :{cnt}, "startTime": 2000, "endTime": 10000000, "source": 153, "destination": 283, "bizType": "2"  }} """ for cnt in range(1, flow_num)]
 s = ",".join(s)
 # print(s)
 
